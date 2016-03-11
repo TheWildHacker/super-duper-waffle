acff8583ad2e1d62328d4d6ac95d0ed7060dd849
Bram van der Hage <b.vdhgithub@gmail.com>
2016-03-11 19:31:31 +0100

    Added some new documentation

--- Python/port_scanner.py
+++ Python/port_scanner.py
@@ -4,3 +4,15 @@
 # Author : TheWildHacker
 # Description : Really easy and short port scanner
 
+import socket
+
+ip = raw_input("Enter the IP address: ")
+port = input("Enter the port number: ")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if sock.connect_ex((ip,port)):
+   print "Port:", port, "is closed"
+else:
+   print "Port:", port, "is open"
+
